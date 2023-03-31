0df871a2e8fec4f1c945acb268fcce6bf0ae8e15
mdmarti <miles.martinez@duke.edu>
2023-03-31 12:49:10 -0400

    adding error metrics

--- experiments.py
+++ experiments.py
@@ -5,10 +5,11 @@ import pandas as pd
 from EDMD.hua import approximate_koopman
 
 def loadData(filepath): 
-    # loads our data. assumes they are stored in .csv files,
-    # using latin1 encoding
-    # first column of the csv is time, second column is signal
-
+    """
+    loads our data. assumes they are stored in .csv files,
+    using latin1 encoding
+    first column of the csv is time, second column is signal
+    """
     data = pd.read_csv(filepath,encoding='latin1',usecols=[0,1])
     time = np.array(data.iloc[:,0])
     signal = np.array(data.iloc[:,1])
@@ -19,11 +20,12 @@ def loadData(filepath):
     return time,signal, fs
 
 def splitData(time,signal,fs,num_segments = 10, train_segment_length = 6,test_segment_length=1):
-    # takes in time, signal, and sampling rate of data as first arguments
-    # also takes in: number of train/test segments we want (integer)
-    # and train segment length (in seconds) and test segment length (in seconds)
-    # test segment will always follow directly after train segment!
-
+    """ 
+    takes in time, signal, and sampling rate of data as first arguments
+    also takes in: number of train/test segments we want (integer)
+    and train segment length (in seconds) and test segment length (in seconds)
+    test segment will always follow directly after train segment!
+    """
     allSegs = []
 
     nPoints = len(signal)
@@ -49,6 +51,19 @@ def splitData(time,signal,fs,num_segments = 10, train_segment_length = 6,test_se
         allSegs.append(data)
 
     return data
+
+def calcMetrics(xhat,x):
+    """
+    takes as input predictions (xhat), data (x). computes and returns
+    MSE, l2 norm, and linf of error between true, predicted data.
+    """
+    err = xhat - x
+    mse = np.nanmean(err**2)
+    l2 = np.sqrt(np.sum(err ** 2))
+    linf = np.amax(err)
+
+    return mse,l2,linf
+
 
 
 
