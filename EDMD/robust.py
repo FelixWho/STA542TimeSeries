c2a373efab3ff536b8e3b082fc9448e04305eecd
felixwho <twodragonhu@gmail.com>
2023-04-13 19:51:28 -0400

    closer to the correct math

--- EDMD/robust.py
+++ EDMD/robust.py
@@ -1,4 +1,7 @@
-'''Hua et al. kernel trick EDMD combined with Sinha et al. Online Robust Koopman https://arxiv.org/pdf/2212.05259.pdf'''
+'''
+Hua et al. kernel trick EDMD combined with Sinha et al. Online Robust Koopman https://arxiv.org/pdf/2212.05259.pdf
+Update: I think this is a better rendition of online Koopman: https://arxiv.org/pdf/1909.12520.pdf
+'''
 
 import math
 import numpy as np
@@ -14,7 +17,7 @@ class RobustEDMD:
         '''
         M: data window, X = [x_1, ..., x_M]
         delta: regularization parameter
-        sigma_2: RBF kernel width
+        sigma_2: RBF kernel width, default 100 according to HT paper
         '''
         self.A = np.zeros((M, M)) # initialize as M x M 0-matrix
         self.G_hat = delta * np.identity(M) # initialize as delta * identity matrix
@@ -22,39 +25,63 @@ class RobustEDMD:
         self.K = self.G_hat_inv @ self.A # This should just equal the M x M 0-matrix, but whatever
         self.sigma_2 = sigma_2
 
-    def update_koopman_and_forecast_point(self, new_data):
-        # This is all wrong
+    def update_koopman_and_forecast_point(self, x, y):
+        '''
+        x is column vector with shape M x 1: [x1, ..., xM]
+        y is column vector with shape M x 1: [x2, ..., xM+1]
+        Uxy has shape 2M x 1
+        tmp has shape 2M x 2M
+        phix_phix_t/phiy_phix_t have shape M x M
+        self.G_hat/self.G_hat_inv has shape M x M
+        self.A has shape M x M
+
+        '''
+        M = np.shape(x)[0]
+
+        assert np.shape(y)[0] == M
+
+        Uxy = np.vstack([x, y])
+
         # Kernel trick
-        phi_t_phi = np.exp(-1/self.sigma_2 * distance.squareform(distance.pdist(new_data)))
+        tmp = np.exp(-1/self.sigma_2 * distance.squareform(distance.pdist(Uxy)))
+
+        phix_phix_t = tmp[:M, :M]
+        phiy_phix_t = tmp[M:, :M]
 
         # Based off Sinha et al.
         # Compute the denominator of G_hat_inv_m+1
-        # TODO: FELIX DOUBLE CHECK THE MATH HERE
-        denom = 1 + np.sum(np.transpose(phi_t_phi) @ self.G_hat_inv)
+        denom = 1 + np.sum(np.transpose(phix_phix_t) @ self.G_hat_inv)
 
         # Compute updated G_hat_m --> G_hat_m+1
-        self.G_hat += phi_t_phi
+        self.G_hat += phix_phix_t
         
         # Compute updated G_hat_inv_m --> G_hat_inv_m+1
-        self.G_hat_inv -= (1/denom) * (self.G_hat_inv @ phi_t_phi @ self.G_hat_inv)
+        self.G_hat_inv -= (1/denom) * (self.G_hat_inv @ phix_phix_t @ self.G_hat_inv)
         
         # Compute updated A_m --> A_m+1
-        self.A += phi_t_phi
+        self.A += phiy_phix_t
         
         # Update Koopman operator K_m --> K_m+1
         self.K = self.G_hat_inv @ self.A
         
         # Forecast new data point using updated Koopman operator
         # Based off Hua et al.
-        mu, Vhat = np.linalg.eig(self.K)
+        mu, Vhat = np.linalg.eig(self.K) # mu is koopman eigenvalues
 
         # We need to find Sigma2, Q = eig(G_hat), but we only have G_hat_inv <--- EDIT: we do actually track G_hat, my bad
         # Linear algebra fact: for every eigenvector, eigenvalue pair (v, lambda) of G_hat,
         # (v, 1/lambda) is an eigenvector, eigenvalue pair of G_hat_inv
         Sigma2, Q = np.linalg.eig(self.G_hat_inv)
         SigmaPINV = np.sqrt(Sigma2)
+        SigmaPINV_diag = np.diag(SigmaPINV)
 
-        # Got the koopman eigenvalues
+        # Get the koopman eigenfunctions
+        Uga = np.vstack([self.G_hat, self.A])
+        Phixy = Uga @ Q @ SigmaPINV_diag @ Vhat # TODO: I'm actually unsure about the dimensionality of Q, SigmaPINV
+
+        # Get the koopman modes
+        xi = np.linalg.pinv(Phixy) @ Uxy
+
 
 def main():
     pass
---
+++ benchmark.py
@@ -0,0 +1,31 @@
+import time
+import numba
+import numpy as np
+from numpy.linalg import pinv as np_pinv
+import jax.numpy as jnp
+from scipy.linalg import pinvh
+#from scipy.linalg import pinv2 as scipy_pinv2
+
+@numba.njit
+def np_jit_pinv(A):
+  return np_pinv(A)
+
+matrix = np.random.rand(300, 300)
+for pinv in [np_pinv, np_jit_pinv, jnp.linalg.pinv, np.linalg.inv]:
+    start = time.time()
+    pinv(matrix)
+    print(f'{pinv.__module__ +"."+pinv.__name__} took {time.time()-start:.3f}')
+
+matrix_symm = (matrix + matrix.T)/2
+for eig in [np.linalg.eig, np.linalg.eigh]:
+    start = time.time()
+    a, b = eig(matrix_symm)
+    print(np.shape(a))
+    print(np.shape(b))
+    print(f'{eig.__module__ +"."+eig.__name__} took {time.time()-start:.3f}')
+
+# Takeaways
+# For pinv small matrix, use np. For pinv big matrix use jax.
+# For pinv symmetric matrix, use scipy pinvh maybe, numpy pinv is still pretty fast.
+# np.linalg.inv is so fast though........
+# For eig, try out np linalg eigh for symmetric matrices maybe
